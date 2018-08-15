28465483b25fed2756183795dd57616b631bab64
Paul M. Lyon <pmlyon@ifai.com>
2018-08-15 09:11:28 -0500

    Updated success template.

--- resources/views/includes/success.blade.php
+++ resources/views/includes/success.blade.php
@@ -1,9 +1,11 @@
 @if ($flash = session('message'))
-  <section class='row'>
-    <div class='col mx-auto mx-2'>
-      <div class='alert alert-success' id='alert' role='alert'>
-        {{ $flash }}
-      </div>
-    </div>
-  </section>
+    <section class='container'>
+        <div class="row">
+            <div class='col mx-auto mx-2'>
+                <div class='alert alert-success' id='alert' role='alert'>
+                    {{ $flash }}
+                </div>
+            </div>
+        </div>
+    </section>
 @endif
