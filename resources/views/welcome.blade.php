85a5fb50f4e653b81121bb675479ab805805d5ab
Paul M. Lyon <pmlyon@ifai.com>
2018-08-15 09:51:57 -0500

    Added background image to welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex-center my-auto">
-        <div class="title">
-            Outdoor Journal
+    <div class="content">
+        <div class="flex-center my-auto">
+            <div class="title">
+                Enjoy the Outdoors!
+            </div>
         </div>
     </div>
 @endsection
