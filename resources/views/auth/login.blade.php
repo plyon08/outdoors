e75920d0891cbd19f189f1d03b688c77d5a7872f
Paul M. Lyon <pmlyon@ifai.com>
2018-08-15 13:21:13 -0500

    Changed how login and register templates looked.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+    <div class="row">
+        <div class="col-12">
+            <div class="">
+                <div class="">{{ __('Login') }}</div>
+                <hr>
+                <div class="">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                         @csrf
 
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-outline-success">
                                     {{ __('Login') }}
                                 </button>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
+    <div class="row ">
+        <div class="col-12">
+            <div class="">
+                <div class="">{{ __('Register') }}</div>
+                <hr>
+                <div class="">
                     <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
                         @csrf
 
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-outline-success">
                                     {{ __('Register') }}
                                 </button>
                             </div>
