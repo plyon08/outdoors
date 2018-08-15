ac1be18aae728dec681108f3af62f60a26c0e5e2
Paul M. Lyon <pmlyon@ifai.com>
2018-08-15 13:44:46 -0500

    Changed redirect after login.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/profile';
+    protected $redirectTo = '/journal';
 
     /**
      * Create a new controller instance.
